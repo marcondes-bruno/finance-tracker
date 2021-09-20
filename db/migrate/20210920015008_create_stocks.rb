8874b380920a1c0dec1bbe7ee719933567bfc9a6
marcondes-bruno <62970243+marcondes-bruno@users.noreply.github.com>
2021-09-20 14:37:22 +1200

    Add stock model

---
+++ db/migrate/20210920015008_create_stocks.rb
@@ -0,0 +1,11 @@
+raiclass CreateStocks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :stocks do |t|
+      t.string :ticker
+      t.string :name
+      t.decimal :last_price
+
+      t.timestamps
+    end
+  end
+end
